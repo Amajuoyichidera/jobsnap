570ff2d7b0541110f272eb8a05398ba18bc62b65
Amajuoyichidera <amajuoyichideradavid@gmail.com>
2024-07-08 10:43:56 +0100

    first version

--- Screens/JobApplyForm.js
+++ Screens/JobApplyForm.js
@@ -1,22 +1,24 @@
 import React, { useState } from 'react';
-import { View, Text, TextInput, Button, Modal, StyleSheet, TouchableOpacity, Alert } from 'react-native';
+import { View, Text, TextInput, Button, Modal, StyleSheet, TouchableOpacity, Alert, ScrollView } from 'react-native';
 import * as DocumentPicker from 'expo-document-picker';
 import { useFonts } from 'expo-font';
+import LottieView from 'lottie-react-native';
+import { Picker } from '@react-native-picker/picker';
 
 const JobApplyForm = ({ navigation }) => {
-    const [loaded] = useFonts({
-        'Poppins-Bold': require('../assets/fonts/Poppins-Bold.ttf'),
-      });
-    
-      // Check if fonts are loaded
-      if (!loaded) {
-        return null; // Return null or a loading indicator until fonts are loaded
-      }
+  const [loaded] = useFonts({
+    'Poppins-Bold': require('../assets/fonts/Poppins-Bold.ttf'),
+  });
+
+  if (!loaded) {
+    return null; // Return null or a loading indicator until fonts are loaded
+  }
 
   const [name, setName] = useState('');
   const [email, setEmail] = useState('');
   const [cv, setCv] = useState(null);
   const [number, setNumber] = useState('');
+  const [gender, setGender] = useState(''); // Add state for gender
   const [isSuccessModalVisible, setIsSuccessModalVisible] = useState(false);
 
   const handleFilePicker = async () => {
@@ -33,12 +35,8 @@ const JobApplyForm = ({ navigation }) => {
   };
 
   const handleSubmit = () => {
-    if (name && email && number && cv) {
+    if (name && email && number && gender && cv) {
       setIsSuccessModalVisible(true);
-    //   setCv('');
-    //   setEmail('');
-    //   setName('');
-    //   setNumber('');
     } else {
       Alert.alert('Please fill all fields and select a CV.');
     }
@@ -47,13 +45,11 @@ const JobApplyForm = ({ navigation }) => {
   const closeModal = () => {
     setIsSuccessModalVisible(false);
     navigation.navigate('Jobs');
-  }
+  };
 
   return (
-    <View style={styles.container}>
-        <Text style={styles.info}>
-            Contact Info
-        </Text>
+    <ScrollView style={styles.container}>
+      <Text style={styles.info}>Contact Info</Text>
       <Text style={styles.label}>Name:</Text>
       <TextInput
         style={styles.input}
@@ -71,7 +67,7 @@ const JobApplyForm = ({ navigation }) => {
         keyboardType="email-address"
       />
 
-     <Text style={styles.label}>Number:</Text>
+      <Text style={styles.label}>Number:</Text>
       <TextInput
         style={styles.input}
         placeholder="Enter your Phone Number"
@@ -80,19 +76,30 @@ const JobApplyForm = ({ navigation }) => {
         keyboardType='numeric'
       />
 
+      <Text style={styles.label}>Gender:</Text>
+      <Picker
+        selectedValue={gender}
+        style={styles.picker}
+        onValueChange={(itemValue) => setGender(itemValue)}
+      >
+        <Picker.Item label="Select Gender" value="" />
+        <Picker.Item label="Male" value="male" />
+        <Picker.Item label="Female" value="female" />
+      </Picker>
+
       <Text style={styles.label}>CV:</Text>
       <TouchableOpacity style={styles.button} onPress={handleFilePicker}>
         <Text style={styles.buttonText}>{cv ? cv : 'Select CV'}</Text>
       </TouchableOpacity>
 
       <View style={styles.btn2Con}>
-      <TouchableOpacity style={[styles.button, {backgroundColor: 'black'}]} onPress={handleSubmit}>
-        <Text style={styles.buttonText}>Send Application</Text>
-      </TouchableOpacity>
+        <TouchableOpacity style={[styles.button, { backgroundColor: 'black' }]} onPress={handleSubmit}>
+          <Text style={styles.buttonText}>Send Application</Text>
+        </TouchableOpacity>
 
-      <TouchableOpacity style={[styles.button, {backgroundColor: '#FF6347'}]} onPress={() => navigation.navigate('Jobs')}>
-        <Text style={styles.buttonText}>Cancel</Text>
-      </TouchableOpacity>
+        <TouchableOpacity style={[styles.button, { backgroundColor: '#FF6347' }]} onPress={() => navigation.navigate('Jobs')}>
+          <Text style={styles.buttonText}>Cancel</Text>
+        </TouchableOpacity>
       </View>
 
       <Modal
@@ -103,19 +110,26 @@ const JobApplyForm = ({ navigation }) => {
       >
         <View style={styles.modalContainer}>
           <View style={styles.modalContent}>
+            <LottieView
+              style={styles.animation}
+              source={require('../assets/sucess.json')} // path to your Lottie JSON file
+              autoPlay
+              loop={false}
+            />
             <Text style={styles.modalText}>Application Successful!</Text>
             <Button title="Close" onPress={() => navigation.navigate('Jobs')} />
           </View>
         </View>
       </Modal>
-    </View>
+    </ScrollView>
   );
 };
 
 const styles = StyleSheet.create({
   container: {
-    marginTop: 90,
+    marginTop: 50,
     padding: 20,
+    marginBottom: 100
   },
   label: {
     fontSize: 18,
@@ -130,7 +144,16 @@ const styles = StyleSheet.create({
     borderRadius: 5,
     marginBottom: 20,
     fontFamily: 'Poppins',
-    color: '#888',        
+    color: '#888',
+  },
+  picker: {
+    borderWidth: 1,
+    borderColor: '#ccc',
+    padding: 10,
+    borderRadius: 5,
+    marginBottom: 20,
+    fontFamily: 'Poppins',
+    color: '#888',
   },
   button: {
     backgroundColor: '#007BFF',
@@ -142,27 +165,33 @@ const styles = StyleSheet.create({
   buttonText: {
     color: '#fff',
     fontWeight: 'bold',
-    fontFamily: 'Poppins'
+    fontFamily: 'Poppins',
   },
   modalContainer: {
     flex: 1,
     justifyContent: 'center',
     alignItems: 'center',
-    backgroundColor: 'rgba(0,0,0,0.5)',
+    backgroundColor: 'rgba(0, 0, 0, 0.5)',
   },
   modalContent: {
-    backgroundColor: '#fff',
-    padding: 20,
+    width: '80%',
+    backgroundColor: 'white',
     borderRadius: 10,
+    padding: 20,
     alignItems: 'center',
   },
+  animation: {
+    width: 150,
+    height: 150,
+  },
   modalText: {
-    fontSize: 18,
-    marginBottom: 20,
-    fontFamily: 'Poppins',
+    fontSize: 20,
+    fontWeight: 'bold',
+    marginVertical: 20,
+    textAlign: 'center',
   },
   btn2Con: {
-    marginTop: 150,
+    marginTop: 50,
   },
   info: {
     fontSize: 30,
@@ -171,7 +200,7 @@ const styles = StyleSheet.create({
     paddingBottom: 30,
     textTransform: 'uppercase',
     fontFamily: 'Poppins',
-  }
+  },
 });
 
 export default JobApplyForm;
